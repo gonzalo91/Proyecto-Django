ac7db64f5d18b67e7931e34705ed597026769b17
Gonzalo <gonzalo.adriancr@gmail.com>
2020-06-07 19:42:14 -0500

    Arreglo error en context processors, usuario anonimo

--- Apps/Carrito/context_processors.py
+++ Apps/Carrito/context_processors.py
@@ -3,8 +3,10 @@
 
 def cart(request):
     from .models import Cart, CartItem
+    cart_items_test = []
 
-    cart_items_test = CartItem.objects.filter(cart__in = Cart.objects.filter(user = request.user))
+    if(request.user.is_authenticated):
+        cart_items_test = CartItem.objects.filter(cart__in = Cart.objects.filter(user = request.user))
     
     return { 'cart_items' : cart_items_test }
 
